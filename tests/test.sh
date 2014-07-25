41f42360b8217d90d588913c299d9b44a13b22e8
DQNEO <dqneoo@gmail.com>
2014-07-26 01:39:17 +0900

    add unit test

---
+++ tests/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cmd=./ltsview
+
+counter=1
+is () {
+    ret=$1
+    exp=$2
+
+    if [[ "$ret" = "$exp" ]] ; then
+	echo ok $counter
+	counter=$(expr $counter +  1 )
+    else
+	echo not ok
+	echo "$ret"
+	echo "$exp"
+    fi
+
+}
+echo "1..2"
+ret=$(printf 'l1:v1\tl2:v2\nl3:v3' | $cmd)
+exp="=========
+l1: v1
+l2: v2
+=========
+l3: v3"
+
+is "$ret" "$exp"
+
+ret=$(printf 'l1:v1\tl2:v2\nl3:v3' | $cmd -k l1,l3)
+exp="=========
+l1: v1
+=========
+l3: v3"
+
+is "$ret" "$exp"
